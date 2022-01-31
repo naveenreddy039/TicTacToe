7b748bc9ad927fb64c06f80c8dff0550295f20a6
naveen <naveenreddy.ko7@gmail.com>
2022-01-31 19:24:24 +0530

    add Project Files

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int[] arr = new int[n];
+            for(int i= 0; i < n; i++)
+            {
+                arr[i] = i;
+
+            }
+            Console.WriteLine(arr[i]);
+            TicTacToeBoard board=new TicTacToeBoard();
+        }
+    }
+}
